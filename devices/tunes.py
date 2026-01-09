e97e8066e657d8cbfa4668551ca9c324742b7a83
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-01-09 12:33:54 +0100

    [TASK] tune device exporting data using tune model
    Todo: move tune model to accml_lib

--- devices/tunes.py
+++ devices/tunes.py
@@ -1,9 +1,12 @@
 import time
 
+import jsons
 from bluesky.protocols import Reading
+from event_model import DataKey
 from ophyd_async.core import StandardReadable, AsyncStatus
 from ophyd_async.epics.core import epics_signal_r
 
+from accml.app.tune.model import Tune
 from accml.core.utils.ophyd_async.new_value import wait_for_new_value
 
 
@@ -18,10 +21,43 @@ class TuneSignal(StandardReadable):
         await wait_for_new_value(self.sig, timeout=5)
         return await super().read()
 
-
-class Tunes(StandardReadable):
+class TunesTransversal(StandardReadable):
     def __init__(self, prefix, *, name):
         with self.add_children_as_readables():
             self.x = TuneSignal(f"{prefix}:x", name=f"{name}-x")
             self.y = TuneSignal(f"{prefix}:y", name=f"{name}-y")
         super().__init__(name=name)
+
+    async def describe(self) -> dict[str, DataKey]:
+        tmp = await super().describe()
+        d = {
+            self.name: dict(shape=[2], dtype="array", source=""),
+        }
+        return {**tmp, **d}
+
+    @AsyncStatus.wrap
+    async def read(self) -> dict[str, Reading]:
+        tmp =  await super().read()
+        x = tmp[f"{self.name}-x-sig"]
+        y = tmp[f"{self.name}-y-sig"]
+        # timestamps often allow no add but sub ...
+        # therefore this extra turn
+        dt = y["timestamp"] - x["timestamp"]
+        severity = max(x["alarm_severity"], y["alarm_severity"])
+        # return a proper data model here!
+        d = {
+            self.name : dict(
+                value=jsons.dump(Tune(x=x["value"], y=y["value"])),
+                timestamp=x["timestamp"] + dt/2.0,
+                alarm_severity=severity
+            )
+        }
+        return {**tmp, **d}
+
+
+class Tunes(StandardReadable):
+    def __init__(self, prefix, *, name):
+        with self.add_children_as_readables():
+            self.transversal = TunesTransversal(prefix, name=f"{name}-transversal")
+        super().__init__(name=name)
+
