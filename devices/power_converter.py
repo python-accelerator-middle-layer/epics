01024b0678df5464fd6ab782e6b00054b5dd0356
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-01-15 08:48:03 +0100

    [FIX] device: __getattr__ interfers with bluesky functionality
    Need to find a better solution

--- devices/power_converter.py
+++ devices/power_converter.py
@@ -4,8 +4,4 @@ from accml.custom.epics.devices.utils import PVPositionerIsClose
 class PowerConverter(PVPositionerIsClose):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-    def __getattr__(self, item):
-        if item == "set_current":
-            return self.setpoint
-        raise AssertionError("don't know how to handle set point", item)
+        self.set_current = self.setpoint
